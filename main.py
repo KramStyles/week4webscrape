e21890467b39a07b8b36e5a997e7da08c241f8c5
kramstyles <mark.eke@decagon.dev>
2022-03-05 00:42:54 +0100

    working with parameters in beautiful soup

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ url = 'https://news.ycombinator.com/news'
 req = request('GET', url)
 soup = BeautifulSoup(req.text, 'html.parser')
 
-with open('soup.html', 'w') as my_soup:
-    my_soup.write(str(soup))
+# with open('soup.html', 'w') as my_soup:
+#     my_soup.write(str(soup))
+
+anchors = soup.body.find_all('a')
+print(soup.title)
+print(soup.find('a')) # finds the first a tag
+print(soup.find(id='votelinks'))
